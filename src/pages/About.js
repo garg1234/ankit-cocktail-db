e5e24957e5dde279e4da344b9f4cc6649f163f93
Ankit Garg <gargankit1061997@gmail.com>
2020-08-08 14:40:22 +0530

    Init Commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+        consequuntur officia voluptatibus quia expedita, ipsum reiciendis nulla
+        tenetur impedit, voluptates incidunt. Odio neque, porro minus labore
+        autem eius. Ullam, iste? ipsum dolor sit, amet consectetur adipisicing
+        elit. Vel consequuntur officia voluptatibus quia expedita.
+      </p>
+    </section>
+  );
+}
